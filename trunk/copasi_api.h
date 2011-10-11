1ab30467b5e7ea33997ef889631afbfc03232cca
dchandran1@gmail.com <dchandran1@gmail.com@9fcaf49e-b9de-1339-30a5-b36a66ef9904>
2011-10-11 21:55:02 +0000

    added cAssignmentRulesReordering for testing what happens when assignment rules are not reordered

--- trunk/copasi_api.h
+++ trunk/copasi_api.h
@@ -789,6 +789,19 @@ TCAPIEXPORT void cSetOptimizerMutationRate(double);
 */
 TCAPIEXPORT void cSetOptimizerCrossoverRate(double);
 
+/*! 
+ \brief do not modify assignment rules
+             warning: disabling this may cause numerical errors in time-course simulations
+ \ingroup cleanup
+*/
+TCAPIEXPORT void cDisableAssignmentRuleReordering();
+
+/*! 
+ \brief modify assignment rules to avoid dependencies between assignment rules (default)
+ \ingroup cleanup
+*/
+TCAPIEXPORT void cEnableAssignmentRuleReordering();
+
 END_C_DECLS
 #endif
 
